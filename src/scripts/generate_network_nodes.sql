4c6eec1da0c23d93e016748d399a4d4e504991e3
aaronfraint <aaronfraint@icloud.com>
2021-12-17 11:02:21 -0500

    keep 1 instance of each point

--- src/scripts/generate_network_nodes.sql
+++ src/scripts/generate_network_nodes.sql
@@ -1,4 +1,10 @@
-create table lts_nodes as
-    (select source as nodeid,
-                      st_startpoint((st_dump(geom)).geom) as geom
-     from lts_ways)
+create table lts_nodes as (
+    with temp_table as (
+        select source as nodeid,
+               st_startpoint((st_dump(geom)).geom) as geom
+        from lts_ways
+    )
+    select nodeid, geom
+    from temp_table
+    group by nodeid, geom
+)
