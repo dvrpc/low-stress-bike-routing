b4bbc6b652b5d0c7e020aefce6c7d8d6af3e6c3d
aaronfraint <aaronfraint@icloud.com>
2022-02-09 09:24:02 -0500

    add route to find the network node closest to a lon/lat

--- app/app/main.py
+++ app/app/main.py
@@ -24,9 +24,14 @@ app.add_middleware(
 )
 
 
-
 @app.get(URL_PREFIX + "/route/")
-async def get_lowest_stress_route(start: int = Query(None), end: int = Query(None),):
+async def get_lowest_stress_route(
+    start: int = Query(None),
+    end: int = Query(None),
+):
+    """
+    Compute a pgrouting route solve using the request start and end node ID values
+    """
     query = f"""
         select
             seq,
@@ -59,5 +64,41 @@ async def get_lowest_stress_route(start: int = Query(None), end: int = Query(Non
     return await postgis_query_to_geojson(
         query,
         ["seq", "id", "geometry"],
+        DATABASE_URL,
+    )
+
+
+@app.get(URL_PREFIX + "/nearest-node/")
+async def get_nearest_node_to_lon_lat(
+    lon: float = Query(None),
+    lat: float = Query(None),
+    dist_meters: float = Query(None),
+):
+    """
+    Find the nearest network node to a given lon/lat, within the requested distance (in meters)
+    """
+    query = f"""
+        with user_point as (
+            select ST_SetSRID(
+                    ST_MakePoint({lon}, {lat}),
+                    4326)::geography as geog
+        )
+        select
+            n.nodeid,
+            n.geom as geometry,
+            ST_Distance(n.geom::geography, p.geog) as match_distance
+        from
+            lts_nodes n,
+            user_point p
+        where
+            ST_DWithin(p.geog, n.geom::geography, {dist_meters})
+        order by
+            ST_Distance(n.geom::geography, p.geog) asc
+        limit 1
+    """
+
+    return await postgis_query_to_geojson(
+        query,
+        ["nodeid", "geometry", "match_distance"],
         DATABASE_URL,
     )
